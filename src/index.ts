f6fa8e4a7c128ff13cc64d65fb1483457e8202b7
Ivan <ipykaloi@gmail.com>
2024-01-02 11:09:19 +0200

    added type effect

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
+import { descriptions } from "./scripts/constnts";
 import "./scss/styles.scss";
+
+addEventListener("load", () => {
+  const article = document.getElementById("description");
+
+  for (let i = 0; i < descriptions.length; i++) {
+    let text = descriptions[i];
+    let l = 0;
+    var speed = 20;
+    const p = document.createElement("p");
+    article.appendChild(p);
+
+    print(l, speed, text, p);
+    i++;
+  }
+
+  function print(i: number, speed: number, text: string, element: HTMLElement) {
+    if (i < text.length) {
+      element.innerHTML += text.charAt(i);
+      i++;
+      setTimeout(() => print(i, speed, text, element), speed);
+    }
+  }
+});
---
+++ src/scripts/constnts.ts
@@ -0,0 +1,32 @@
+export const descriptions = [
+  `👋 Hello there! I'm a seasoned software engineer with over 5 years of
+      hands-on experience in crafting web applications.`,
+  ,
+  `💻 My expertise lies in web development, where I thrive in the HTML,
+      CSS, and JavaScript trio. I'm passionate about creating seamless and
+      visually appealing user experiences that leave a lasting impression.`,
+  ,
+  `🅰️ On the front-end, I'm well-versed in the Angular framework,
+      employing it to build robust and dynamic user interfaces. I have an
+      eye for design and a commitment to delivering responsive and intuitive
+      applications.`,
+  ,
+  `🚀 Beyond the client-side, I've extended my skills to the server
+    realm. I have a solid foundation in server-side development using
+    technologies like Node.js and NestJS. Databases like MongoDB are my
+    playground, where I ensure data integrity and efficiency.`,
+  ,
+  `🌐 Whether it's shaping the client's journey with a sleek UI or
+    orchestrating server-side functionalities, I bring a holistic approach
+    to web development. My goal is to create software that not only meets
+    technical requirements but also exceeds user expectations.`,
+  ,
+  `📱 In addition to web applications, I bring valuable experience in
+    building mobile applications. I've successfully developed Apps for
+    mobile and tablet (Android, IOS, Windows) devices.`,
+  ,
+  `🌱 Always eager to explore new technologies and methodologies, I stay
+    up-to-date with the latest trends in the ever-evolving tech landscape.
+    Let's build something extraordinary together! 🚀`,
+  ,
+];
